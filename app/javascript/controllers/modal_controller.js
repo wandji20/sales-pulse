7ecfb676f0a41f27432fd346c3f66d7db6a9a40d
wandji20 <wbertrand10@gmail.com>
2024-11-11 02:41:30 +0100

    Add support for lazy loading modal content and apply to new product variant button

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -1,25 +1,43 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ['backdrop']
+  static targets = ['backdrop', 'content']
+  static values = { lazyLoadContent: { type: Boolean, default: false }}
 
   connect() {
     this.element.addEventListener('modal:open', this.#openModal.bind(this));
-    document.addEventListener('modal:close', this.#closeModal.bind(this));
+    document.addEventListener('modal:close', this.closeModal.bind(this));
+    document.addEventListener('turbo:submit-end', this.#closeFormModal.bind(this));
   }
 
   disconnect() {
     this.element.removeEventListener('modal:open', this.#openModal.bind(this));
-    document.removeEventListener('modal:close', this.#closeModal.bind(this));
+    document.removeEventListener('modal:close', this.closeModal.bind(this));
   }
 
-  #openModal(event) {
+  async #openModal(event) {
     this.element.classList.remove('hidden');
     document.querySelector('body').classList.add('overflow-y-hidden');
+
+    if (this.lazyLoadContentValue) {
+      const url = event.detail.triggerElement?.dataset?.url
+      if (!url) return;
+
+      const response = await fetch(url, { headers: { "Accept": "application/json" } });
+      const data = await response.json();
+      this.contentTarget.innerHTML = data.html;
+    }
   }
 
-  #closeModal() {
+  closeModal() {
     this.element.classList.add('hidden');
+    this.contentTarget.innerHTML = '';
     document.querySelector('body').classList.remove('overflow-y-hidden');
+  }
+
+  #closeFormModal(event) {
+    if (event.detail.success) {
+      this.closeModal();
+    }
   }
 }
--- config/importmap.rb
+++ config/importmap.rb
@@ -3,5 +3,6 @@
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "mixins/useTriggerOpenAndClose", to: "mixins/useTriggerOpenAndClose.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
