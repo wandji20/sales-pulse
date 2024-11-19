df5df21aa5736d0b465a80c020eae3704302cccd
wandji20 <wbertrand10@gmail.com>
2024-11-19 22:15:09 +0100

    Add layout for new sales record form

--- app/helpers/records_helper.rb
+++ app/helpers/records_helper.rb
@@ -2,4 +2,12 @@ module RecordsHelper
   def header_class(header)
     "min-w-[100px] sticky top-0"
   end
+
+  def form_statuses
+    Record.statuses.keys.map { |status| [ status, t("records.sales_form.status.#{status}") ] }
+  end
+
+  def form_categories
+    Record.categories.except(:service).keys.map { |category| [ category, t("records.sales_form.category.#{category}") ] }
+  end
 end
